1ef2fa53635eff07efeb59910a43ff2adaffaca4
Mark Roberts <mhroberts7734@gmail.com>
2024-07-07 01:23:46 -0500

    stuff

---
+++ src/Data/game.country.sql
@@ -0,0 +1,252 @@
+INSERT INTO game.country (name, code)
+SELECT 'Afghanistan' Name, 'AF' Code UNION
+SELECT 'Albania' Name, 'AL' Code UNION
+SELECT 'Algeria' Name, 'DZ' Code UNION
+SELECT 'American Samoa' Name, 'AS' Code UNION
+SELECT 'Andorra' Name, 'AD' Code UNION
+SELECT 'Angola' Name, 'AO' Code UNION
+SELECT 'Anguilla' Name, 'AI' Code UNION
+SELECT 'Antarctica' Name, 'AQ' Code UNION
+SELECT 'Antigua and Barbuda' Name, 'AG' Code UNION
+SELECT 'Argentina' Name, 'AR' Code UNION
+SELECT 'Armenia' Name, 'AM' Code UNION
+SELECT 'Aruba' Name, 'AW' Code UNION
+SELECT 'Australia' Name, 'AU' Code UNION
+SELECT 'Austria' Name, 'AT' Code UNION
+SELECT 'Azerbaijan' Name, 'AZ' Code UNION
+SELECT 'Bahamas (the)' Name, 'BS' Code UNION
+SELECT 'Bahrain' Name, 'BH' Code UNION
+SELECT 'Bangladesh' Name, 'BD' Code UNION
+SELECT 'Barbados' Name, 'BB' Code UNION
+SELECT 'Belarus' Name, 'BY' Code UNION
+SELECT 'Belgium' Name, 'BE' Code UNION
+SELECT 'Belize' Name, 'BZ' Code UNION
+SELECT 'Benin' Name, 'BJ' Code UNION
+SELECT 'Bermuda' Name, 'BM' Code UNION
+SELECT 'Bhutan' Name, 'BT' Code UNION
+SELECT 'Bolivia (Plurinational State of)' Name, 'BO' Code UNION
+SELECT 'Bonaire, Sint Eustatius and Saba' Name, 'BQ' Code UNION
+SELECT 'Bosnia and Herzegovina' Name, 'BA' Code UNION
+SELECT 'Botswana' Name, 'BW' Code UNION
+SELECT 'Bouvet Island' Name, 'BV' Code UNION
+SELECT 'Brazil' Name, 'BR' Code UNION
+SELECT 'British Indian Ocean Territory (the)' Name, 'IO' Code UNION
+SELECT 'Brunei Darussalam' Name, 'BN' Code UNION
+SELECT 'Bulgaria' Name, 'BG' Code UNION
+SELECT 'Burkina Faso' Name, 'BF' Code UNION
+SELECT 'Burundi' Name, 'BI' Code UNION
+SELECT 'Cabo Verde' Name, 'CV' Code UNION
+SELECT 'Cambodia' Name, 'KH' Code UNION
+SELECT 'Cameroon' Name, 'CM' Code UNION
+SELECT 'Canada' Name, 'CA' Code UNION
+SELECT 'Cayman Islands (the)' Name, 'KY' Code UNION
+SELECT 'Central African Republic (the)' Name, 'CF' Code UNION
+SELECT 'Chad' Name, 'TD' Code UNION
+SELECT 'Chile' Name, 'CL' Code UNION
+SELECT 'China' Name, 'CN' Code UNION
+SELECT 'Christmas Island' Name, 'CX' Code UNION
+SELECT 'Cocos (Keeling) Islands (the)' Name, 'CC' Code UNION
+SELECT 'Colombia' Name, 'CO' Code UNION
+SELECT 'Comoros (the)' Name, 'KM' Code UNION
+SELECT 'Congo (the Democratic Republic of the)' Name, 'CD' Code UNION
+SELECT 'Congo (the)' Name, 'CG' Code UNION
+SELECT 'Cook Islands (the)' Name, 'CK' Code UNION
+SELECT 'Costa Rica' Name, 'CR' Code UNION
+SELECT 'Croatia' Name, 'HR' Code UNION
+SELECT 'Cuba' Name, 'CU' Code UNION
+SELECT 'Curaçao' Name, 'CW' Code UNION
+SELECT 'Cyprus' Name, 'CY' Code UNION
+SELECT 'Czechia' Name, 'CZ' Code UNION
+SELECT 'Côte d''Ivoire' Name, 'CI' Code UNION
+SELECT 'Denmark' Name, 'DK' Code UNION
+SELECT 'Djibouti' Name, 'DJ' Code UNION
+SELECT 'Dominica' Name, 'DM' Code UNION
+SELECT 'Dominican Republic (the)' Name, 'DO' Code UNION
+SELECT 'Ecuador' Name, 'EC' Code UNION
+SELECT 'Egypt' Name, 'EG' Code UNION
+SELECT 'El Salvador' Name, 'SV' Code UNION
+SELECT 'Equatorial Guinea' Name, 'GQ' Code UNION
+SELECT 'Eritrea' Name, 'ER' Code UNION
+SELECT 'Estonia' Name, 'EE' Code UNION
+SELECT 'Eswatini' Name, 'SZ' Code UNION
+SELECT 'Ethiopia' Name, 'ET' Code UNION
+SELECT 'Falkland Islands (the) [Malvinas]' Name, 'FK' Code UNION
+SELECT 'Faroe Islands (the)' Name, 'FO' Code UNION
+SELECT 'Fiji' Name, 'FJ' Code UNION
+SELECT 'Finland' Name, 'FI' Code UNION
+SELECT 'France' Name, 'FR' Code UNION
+SELECT 'French Guiana' Name, 'GF' Code UNION
+SELECT 'French Polynesia' Name, 'PF' Code UNION
+SELECT 'French Southern Territories (the)' Name, 'TF' Code UNION
+SELECT 'Gabon' Name, 'GA' Code UNION
+SELECT 'Gambia (the)' Name, 'GM' Code UNION
+SELECT 'Georgia' Name, 'GE' Code UNION
+SELECT 'Germany' Name, 'DE' Code UNION
+SELECT 'Ghana' Name, 'GH' Code UNION
+SELECT 'Gibraltar' Name, 'GI' Code UNION
+SELECT 'Greece' Name, 'GR' Code UNION
+SELECT 'Greenland' Name, 'GL' Code UNION
+SELECT 'Grenada' Name, 'GD' Code UNION
+SELECT 'Guadeloupe' Name, 'GP' Code UNION
+SELECT 'Guam' Name, 'GU' Code UNION
+SELECT 'Guatemala' Name, 'GT' Code UNION
+SELECT 'Guernsey' Name, 'GG' Code UNION
+SELECT 'Guinea' Name, 'GN' Code UNION
+SELECT 'Guinea-Bissau' Name, 'GW' Code UNION
+SELECT 'Guyana' Name, 'GY' Code UNION
+SELECT 'Haiti' Name, 'HT' Code UNION
+SELECT 'Heard Island and McDonald Islands' Name, 'HM' Code UNION
+SELECT 'Holy See (the)' Name, 'VA' Code UNION
+SELECT 'Honduras' Name, 'HN' Code UNION
+SELECT 'Hong Kong' Name, 'HK' Code UNION
+SELECT 'Hungary' Name, 'HU' Code UNION
+SELECT 'Iceland' Name, 'IS' Code UNION
+SELECT 'India' Name, 'IN' Code UNION
+SELECT 'Indonesia' Name, 'ID' Code UNION
+SELECT 'Iran (Islamic Republic of)' Name, 'IR' Code UNION
+SELECT 'Iraq' Name, 'IQ' Code UNION
+SELECT 'Ireland' Name, 'IE' Code UNION
+SELECT 'Isle of Man' Name, 'IM' Code UNION
+SELECT 'Israel' Name, 'IL' Code UNION
+SELECT 'Italy' Name, 'IT' Code UNION
+SELECT 'Jamaica' Name, 'JM' Code UNION
+SELECT 'Japan' Name, 'JP' Code UNION
+SELECT 'Jersey' Name, 'JE' Code UNION
+SELECT 'Jordan' Name, 'JO' Code UNION
+SELECT 'Kazakhstan' Name, 'KZ' Code UNION
+SELECT 'Kenya' Name, 'KE' Code UNION
+SELECT 'Kiribati' Name, 'KI' Code UNION
+SELECT 'Korea (the Democratic People''s Republic of)' Name, 'KP' Code UNION
+SELECT 'Korea (the Republic of)' Name, 'KR' Code UNION
+SELECT 'Kuwait' Name, 'KW' Code UNION
+SELECT 'Kyrgyzstan' Name, 'KG' Code UNION
+SELECT 'Lao People''s Democratic Republic (the)' Name, 'LA' Code UNION
+SELECT 'Latvia' Name, 'LV' Code UNION
+SELECT 'Lebanon' Name, 'LB' Code UNION
+SELECT 'Lesotho' Name, 'LS' Code UNION
+SELECT 'Liberia' Name, 'LR' Code UNION
+SELECT 'Libya' Name, 'LY' Code UNION
+SELECT 'Liechtenstein' Name, 'LI' Code UNION
+SELECT 'Lithuania' Name, 'LT' Code UNION
+SELECT 'Luxembourg' Name, 'LU' Code UNION
+SELECT 'Macao' Name, 'MO' Code UNION
+SELECT 'Madagascar' Name, 'MG' Code UNION
+SELECT 'Malawi' Name, 'MW' Code UNION
+SELECT 'Malaysia' Name, 'MY' Code UNION
+SELECT 'Maldives' Name, 'MV' Code UNION
+SELECT 'Mali' Name, 'ML' Code UNION
+SELECT 'Malta' Name, 'MT' Code UNION
+SELECT 'Marshall Islands (the)' Name, 'MH' Code UNION
+SELECT 'Martinique' Name, 'MQ' Code UNION
+SELECT 'Mauritania' Name, 'MR' Code UNION
+SELECT 'Mauritius' Name, 'MU' Code UNION
+SELECT 'Mayotte' Name, 'YT' Code UNION
+SELECT 'Mexico' Name, 'MX' Code UNION
+SELECT 'Micronesia (Federated States of)' Name, 'FM' Code UNION
+SELECT 'Moldova (the Republic of)' Name, 'MD' Code UNION
+SELECT 'Monaco' Name, 'MC' Code UNION
+SELECT 'Mongolia' Name, 'MN' Code UNION
+SELECT 'Montenegro' Name, 'ME' Code UNION
+SELECT 'Montserrat' Name, 'MS' Code UNION
+SELECT 'Morocco' Name, 'MA' Code UNION
+SELECT 'Mozambique' Name, 'MZ' Code UNION
+SELECT 'Myanmar' Name, 'MM' Code UNION
+SELECT 'Namibia' Name, 'NA' Code UNION
+SELECT 'Nauru' Name, 'NR' Code UNION
+SELECT 'Nepal' Name, 'NP' Code UNION
+SELECT 'Netherlands (the)' Name, 'NL' Code UNION
+SELECT 'New Caledonia' Name, 'NC' Code UNION
+SELECT 'New Zealand' Name, 'NZ' Code UNION
+SELECT 'Nicaragua' Name, 'NI' Code UNION
+SELECT 'Niger (the)' Name, 'NE' Code UNION
+SELECT 'Nigeria' Name, 'NG' Code UNION
+SELECT 'Niue' Name, 'NU' Code UNION
+SELECT 'Norfolk Island' Name, 'NF' Code UNION
+SELECT 'Northern Mariana Islands (the)' Name, 'MP' Code UNION
+SELECT 'Norway' Name, 'NO' Code UNION
+SELECT 'Oman' Name, 'OM' Code UNION
+SELECT 'Pakistan' Name, 'PK' Code UNION
+SELECT 'Palau' Name, 'PW' Code UNION
+SELECT 'Palestine, State of' Name, 'PS' Code UNION
+SELECT 'Panama' Name, 'PA' Code UNION
+SELECT 'Papua New Guinea' Name, 'PG' Code UNION
+SELECT 'Paraguay' Name, 'PY' Code UNION
+SELECT 'Peru' Name, 'PE' Code UNION
+SELECT 'Philippines (the)' Name, 'PH' Code UNION
+SELECT 'Pitcairn' Name, 'PN' Code UNION
+SELECT 'Poland' Name, 'PL' Code UNION
+SELECT 'Portugal' Name, 'PT' Code UNION
+SELECT 'Puerto Rico' Name, 'PR' Code UNION
+SELECT 'Qatar' Name, 'QA' Code UNION
+SELECT 'Republic of North Macedonia' Name, 'MK' Code UNION
+SELECT 'Romania' Name, 'RO' Code UNION
+SELECT 'Russian Federation (the)' Name, 'RU' Code UNION
+SELECT 'Rwanda' Name, 'RW' Code UNION
+SELECT 'Réunion' Name, 'RE' Code UNION
+SELECT 'Saint Barthélemy' Name, 'BL' Code UNION
+SELECT 'Saint Helena, Ascension and Tristan da Cunha' Name, 'SH' Code UNION
+SELECT 'Saint Kitts and Nevis' Name, 'KN' Code UNION
+SELECT 'Saint Lucia' Name, 'LC' Code UNION
+SELECT 'Saint Martin (French part)' Name, 'MF' Code UNION
+SELECT 'Saint Pierre and Miquelon' Name, 'PM' Code UNION
+SELECT 'Saint Vincent and the Grenadines' Name, 'VC' Code UNION
+SELECT 'Samoa' Name, 'WS' Code UNION
+SELECT 'San Marino' Name, 'SM' Code UNION
+SELECT 'Sao Tome and Principe' Name, 'ST' Code UNION
+SELECT 'Saudi Arabia' Name, 'SA' Code UNION
+SELECT 'Senegal' Name, 'SN' Code UNION
+SELECT 'Serbia' Name, 'RS' Code UNION
+SELECT 'Seychelles' Name, 'SC' Code UNION
+SELECT 'Sierra Leone' Name, 'SL' Code UNION
+SELECT 'Singapore' Name, 'SG' Code UNION
+SELECT 'Sint Maarten (Dutch part)' Name, 'SX' Code UNION
+SELECT 'Slovakia' Name, 'SK' Code UNION
+SELECT 'Slovenia' Name, 'SI' Code UNION
+SELECT 'Solomon Islands' Name, 'SB' Code UNION
+SELECT 'Somalia' Name, 'SO' Code UNION
+SELECT 'South Africa' Name, 'ZA' Code UNION
+SELECT 'South Georgia and the South Sandwich Islands' Name, 'GS' Code UNION
+SELECT 'South Sudan' Name, 'SS' Code UNION
+SELECT 'Spain' Name, 'ES' Code UNION
+SELECT 'Sri Lanka' Name, 'LK' Code UNION
+SELECT 'Sudan (the)' Name, 'SD' Code UNION
+SELECT 'Suriname' Name, 'SR' Code UNION
+SELECT 'Svalbard and Jan Mayen' Name, 'SJ' Code UNION
+SELECT 'Sweden' Name, 'SE' Code UNION
+SELECT 'Switzerland' Name, 'CH' Code UNION
+SELECT 'Syrian Arab Republic' Name, 'SY' Code UNION
+SELECT 'Taiwan (Province of China)' Name, 'TW' Code UNION
+SELECT 'Tajikistan' Name, 'TJ' Code UNION
+SELECT 'Tanzania, United Republic of' Name, 'TZ' Code UNION
+SELECT 'Thailand' Name, 'TH' Code UNION
+SELECT 'Timor-Leste' Name, 'TL' Code UNION
+SELECT 'Togo' Name, 'TG' Code UNION
+SELECT 'Tokelau' Name, 'TK' Code UNION
+SELECT 'Tonga' Name, 'TO' Code UNION
+SELECT 'Trinidad and Tobago' Name, 'TT' Code UNION
+SELECT 'Tunisia' Name, 'TN' Code UNION
+SELECT 'Turkey' Name, 'TR' Code UNION
+SELECT 'Turkmenistan' Name, 'TM' Code UNION
+SELECT 'Turks and Caicos Islands (the)' Name, 'TC' Code UNION
+SELECT 'Tuvalu' Name, 'TV' Code UNION
+SELECT 'Uganda' Name, 'UG' Code UNION
+SELECT 'Ukraine' Name, 'UA' Code UNION
+SELECT 'United Arab Emirates (the)' Name, 'AE' Code UNION
+SELECT 'United Kingdom of Great Britain and Northern Ireland (the)' Name, 'GB' Code UNION
+SELECT 'United States Minor Outlying Islands (the)' Name, 'UM' Code UNION
+SELECT 'United States of America (the)' Name, 'US' Code UNION
+SELECT 'Uruguay' Name, 'UY' Code UNION
+SELECT 'Uzbekistan' Name, 'UZ' Code UNION
+SELECT 'Vanuatu' Name, 'VU' Code UNION
+SELECT 'Venezuela (Bolivarian Republic of)' Name, 'VE' Code UNION
+SELECT 'Viet Nam' Name, 'VN' Code UNION
+SELECT 'Virgin Islands (British)' Name, 'VG' Code UNION
+SELECT 'Virgin Islands (U.S.)' Name, 'VI' Code UNION
+SELECT 'Wallis and Futuna' Name, 'WF' Code UNION
+SELECT 'Western Sahara' Name, 'EH' Code UNION
+SELECT 'Yemen' Name, 'YE' Code UNION
+SELECT 'Zambia' Name, 'ZM' Code UNION
+SELECT 'Zimbabwe' Name, 'ZW' Code UNION
+SELECT 'Åland Islands   ' Name, 'AX' Code
+EXCEPT
+SELECT name, code FROM game.country;
--- src/Tables/game.user.sql
+++ src/Tables/game.user.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS game.user (
 	identity_provider_id smallint not null,
 	external_id character varying not null,
 	name character varying not null,
+	avatar character varying not null,
 	PRIMARY KEY(id)
 );
 
